d3f74741d89b8a1ec0ccf982f18496d17af8035b
Matthew Birch <matthew678532@gmail.com>
2017-10-04 22:15:24 +0100

    Defined a method to filter the returned feed from parse feed

--- modules/parse.js
+++ modules/parse.js
@@ -24,3 +24,37 @@ exports.parseFeed = function(url) {
 		})
 	})
 }
+
+/**
+ * filterFeed - a function used to filter the
+ * parsed rss feed data, to include only the data
+ * elements required.
+ * @param  {object} feed an object containing only the
+ * feed data of an rss feed.
+ * @return {object}			 a resolved promise object
+ * containing a filtered rss feed.
+ */
+exports.filterFeed = function(feed) {
+	return new Promise((resolve, reject) => {
+		const filteredFeed = {
+			title: feed.title,
+			description: feed.description,
+			entries: []
+		}
+
+		feed.entries.forEach(entry => {
+			const title = entry.title
+			const description = entry.contentSnippet
+			const pubDate = entry.pubDate
+			const link = entry.link
+
+			filteredFeed.entries.push({
+				title,
+				description,
+				pubDate,
+				link
+			})
+		})
+		resolve(filteredFeed)
+	})
+}
