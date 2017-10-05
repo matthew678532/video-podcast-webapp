5438ae02bac886c12ab7d2bf18fc42aa890278c2
Matthew Birch <matthew678532@gmail.com>
2017-10-05 13:53:40 +0100

    Incorporated caching to reduce server to server requests and increase app efficiency

--- routes/video.js
+++ routes/video.js
@@ -1,21 +1,31 @@
 'use strict'
 
 const router = require('express').Router()
+const cache = require('memory-cache')
 const parse = require('./../modules/parse.js')
 const video = require('./../modules/video.js')
 const util = require('./../modules/helpers/util.js')
 const url = 'http://rss.cnn.com/services/podcasting/studentnews/rss.xml'
 
 const parseRss = function(req, res, next) {
-	parse.parseFeed(url).then(res => {
-		return parse.filterFeed(res)
-	}).then(feed => {
-		res.data = feed
-	}).catch(err => {
-		res.err = err
-	}).then(() => {
+	const key = '__express__@secret@'
+	const duration = 300000
+	const cachedFeed = cache.get(key)
+	if (cachedFeed) {
+		res.data = cachedFeed
 		next()
-	})
+	} else {
+		parse.parseFeed(url).then(res => {
+			return parse.filterFeed(res)
+		}).then(feed => {
+			cache.put(key, feed, duration)
+			res.data = feed
+		}).catch(err => {
+			res.err = err
+		}).then(() => {
+			next()
+		})
+	}
 }
 
 router.use(parseRss)
